41082604473947fe53fce1c1c463d11b3b1658bb
menezesmario <mario082@gmail.com>
2020-12-14 18:42:39 -0300

    limpo de componentes desnecessários 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
     <App />,
@@ -9,4 +9,4 @@ ReactDOM.render(
 );
 
 
-reportWebVitals();
+
